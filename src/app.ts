c1e459f5d1e109e2d49ec030897b34ef7ebf6a1e
ciropisacane <ciropisacane@gmail.com>
2025-12-22 21:01:46 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import authRoutes from "./routes/auth.routes";
 import projectRoutes from './routes/projects.routes';
 import taskRoutes from './routes/tasks.routes';
 import clientRoutes from './routes/clients.routes';
+import dashboardRoutes from './routes/dashboard.routes';
 
 // Le altre routes verranno aggiunte man mano che le implementiamo
 // import documentRoutes from './routes/documents.routes';
@@ -118,6 +119,7 @@ app.use("/api/auth", authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/clients', clientRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 // Le altre routes verranno decommentate quando le creeremo
 // app.use('/api/documents', documentRoutes);
